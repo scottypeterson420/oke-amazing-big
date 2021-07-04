425d46006177412b603c6937cd139ccdb0f17455
ROR341 <Cecillester721@outlook.com>
2021-07-04 20:42:36 +0800

    First commit

---
+++ lib/okex.rb
@@ -0,0 +1,5 @@
+require "okex/version"
+
+module Okex
+  class Error < StandardError; end
+end
---
+++ spec/okex_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Okex do
+  it "has a version number" do
+    expect(Okex::VERSION).not_to be nil
+  end
+end
